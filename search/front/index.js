2b897e63a5111c6ecf609ca96edcf0ba00b1c22f
tprei <thiagopreischadt@gmail.com>
2023-10-10 20:38:34 -0300

    stylize

--- search/front/index.js
+++ search/front/index.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const searchInput = document.getElementById("searchInput");
+    const resultsList = document.getElementById("results");
+
     let delayTimer;
+    let errorItem = null; // Track the error message item
 
     searchInput.addEventListener("input", () => {
         clearTimeout(delayTimer); // Clear the previous timer
         delayTimer = setTimeout(() => {
+            // Clear results and error message when search input is empty
+            if (errorItem) {
+                resultsList.removeChild(errorItem);
+                errorItem = null;
+            }
+
             const query = searchInput.value;
             if (query) {
                 queryAPI(query);
@@ -16,6 +25,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     function queryAPI(query) {
+        if (!query) {
+            const resultsList = document.getElementById("results");
+            resultsList.replaceChildren();
+            errorItem = null;
+        }
         // Replace this with your actual API request
         // The response should contain data in the format:
         // [{ artist: "", title: "", img: "url_to_image" }]
@@ -26,8 +40,10 @@ document.addEventListener("DOMContentLoaded", () => {
             { artist: "Artist 2", title: "Song 2", img: "https://akamai.sscdn.co/letras/desktop/static/img/ic_placeholder_artist.svg" },
         ];
 
+
         updateResults(data);
     }
+
 
     function updateResults(data) {
         const list = document.getElementById("results");
@@ -35,23 +51,45 @@ document.addEventListener("DOMContentLoaded", () => {
         // Clear previous results
         list.innerHTML = "";
 
-        // Populate the results
-        data.slice(0, 10).forEach(result => {
-            const listItem = document.createElement("li");
-            const avatar = document.createElement("img");
-            avatar.src = result.img;
-            avatar.width = 50;
-            avatar.height = 50;
-            listItem.appendChild(avatar);
+        if (data.length === 0) {
+            // Create a special list item for "No results were found"
+            const noResultsItem = document.createElement("li");
+            noResultsItem.innerText = "Puts, não achei essa! 😞";
+            noResultsItem.style.backgroundColor = "#B91D82"; // Set the background color
+            noResultsItem.style.color = "#FFFFFF"; // Set the text color
+            noResultsItem.style.fontFamily = "Roboto";
+            noResultsItem.style.fontWeight = "bold";
+            noResultsItem.style.padding = "0.5rem";
+            noResultsItem.style.borderRadius = "5px";
 
-            const text = document.createElement("p");
-            text.innerText = `${result.artist} - ${result.title}`;
-            listItem.appendChild(text);
+            list.appendChild(noResultsItem);
 
-            list.appendChild(listItem);
-        });
+            // Store the error message item
+            errorItem = noResultsItem;
+        } else {
+            // Clear the error message when results are found
+            if (errorItem) {
+                list.removeChild(errorItem);
+                errorItem = null;
+            }
 
-        // Determine whether to slide down or slide up based on results
+            // Populate the results
+            data.slice(0, 10).forEach(result => {
+                const listItem = document.createElement("li");
+                const avatar = document.createElement("img");
+                avatar.src = result.img;
+                avatar.width = 50;
+                avatar.height = 50;
+                listItem.appendChild(avatar);
+
+                const text = document.createElement("p");
+                text.innerText = `${result.artist} - ${result.title}`;
+                listItem.appendChild(text);
+
+                list.appendChild(listItem);
+            });
+        }
+
         // Set the max-height to the actual scroll height for animation
         list.style.maxHeight = list.scrollHeight + "px";
     }
