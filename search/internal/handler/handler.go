9d02efd9d95a01cbd7f37e09247b6933de6908e6
tprei <thiagopreischadt@gmail.com>
2023-10-10 22:50:46 -0300

    add basic handler

--- search/internal/handler/handler.go
+++ search/internal/handler/handler.go
@@ -1,15 +1,46 @@
 package handler
 
-import "github.com/tprei/semcomp/search/internal/database/json"
+import (
+	"strings"
 
-type Song struct{}
-type Lyrics struct{}
-type Strophe struct{}
+	"github.com/tprei/semcomp/search/internal/database/json"
+)
+
+type Song struct {
+	Title  string `json:"title"`
+	Artist string `json:"artist"`
+
+	Image string `json:"img"`
+	Views string `json:"views"`
+
+	Lyrics `json:"lyrics"`
+}
+
+type Lyrics []Strophe
+
+type Strophe []string
 
 const songsFile = `songs.json`
 
 func filterSongs(songs []Song, query string) []Song {
-	return nil
+	filtered := make([]Song, 0, len(songs))
+
+songs:
+	for _, song := range songs {
+		for _, strophes := range song.Lyrics {
+			for _, verse := range strophes {
+				lowerVerse := strings.ToLower(verse)
+				lowerQuery := strings.ToLower(query)
+
+				if strings.Contains(lowerVerse, lowerQuery) {
+					filtered = append(filtered, song)
+					continue songs
+				}
+			}
+		}
+	}
+
+	return filtered
 }
 
 func Search(query string) ([]Song, error) {
